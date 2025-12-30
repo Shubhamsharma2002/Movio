468156e4c87a9ef6a61eee43bebde27c2ff4c6bc
Shubham Sharma <shubhamtech004@gmail.com>
2025-12-30 23:42:38 +0530

    nultiple work
    use fectch function api calling movie card component home page one layout done

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,18 +1,18 @@
 // import SearchBar from '@/components/Searchbar'
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
 const search = () => {
- return (
-       <View className="flex-1 items-center justify-center">
-       <Text className="text-5xl font-bold text-dark-100">
-         Welcome to Searcg screen!
-       </Text>
-       {/* <SearchBar placeholder='welcome'/> */}
-       </View>
-   )
-}
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl font-bold text-dark-100">
+        Welcome to Searcg screen!
+      </Text>
+      {/* <SearchBar placeholder='welcome'/> */}
+    </View>
+  );
+};
 
-export default search
+export default search;
 
 // const styles = StyleSheet.create({})
