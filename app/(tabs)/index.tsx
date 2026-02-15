041693540a3795a8126b00b536519dbbe48d35fb
Shubham Sharma <shubhamtech004@gmail.com>
2026-02-15 23:40:06 +0530

    css fixing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,6 @@ import {
   View,
 } from "react-native";
 
-
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/Searchbar";
 import { icons } from "@/constants/icons";
@@ -16,16 +15,14 @@ import { fetchMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
 
-
 const Index = () => {
-   const router = useRouter();
+  const router = useRouter();
   const {
     data: movies,
     loading,
     error,
   } = useFetch(() => fetchMovies({ query: "" }));
 
-  
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -33,14 +30,14 @@ const Index = () => {
         className="absolute w-full z-0"
         resizeMode="cover"
       />
-       <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-       <SearchBar
-              onPress={() => {
-                router.push("/search");
-              }}
-              placeholder="Search for a movie"
-            />
+      <SearchBar
+        onPress={() => {
+          router.push("/search");
+        }}
+        placeholder="Search for a movie"
+      />
 
       <ScrollView
         className="flex-1 px-5"
@@ -54,9 +51,7 @@ const Index = () => {
         {loading ? (
           <ActivityIndicator size="large" color="#fff" />
         ) : error ? (
-          <Text className="text-red-400">
-            Failed to load movies
-          </Text>
+          <Text className="text-red-400">Failed to load movies</Text>
         ) : (
           <FlatList
             data={movies}
